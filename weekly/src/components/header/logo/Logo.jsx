53a2d08ce6779da7a08aa289fadc81e62e1e7960
gargmanav <78503190+gargmanav@users.noreply.github.com>
2023-03-16 12:50:12 +0530

    Update Logo.jsx

--- weekly/src/components/header/logo/Logo.jsx
+++ weekly/src/components/header/logo/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo() {
   return (
     <div>
-        <img src="./images/logo.png" alt='laddu' style={{width:'160px',height:'70px'}}></img>
+        <img src="Images/logo.png" alt='laddu' style={{width:'160px',height:'70px'}}></img>
     </div>
   )
 }
